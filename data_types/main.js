eb2f74cd9577a464d6589d20f2ba88c5d041b3fb
unknown <diana.chorna@gmail.com>
2022-01-23 14:21:30 +0200

    data_types

---
+++ data_types/main.js
@@ -0,0 +1,17 @@
+//завдання 1
+let city;
+city = "Київ";
+city = "Львів";
+let address = city;
+alert(address);
+
+//завдання  2
+let n = 3;
+let result = n*n*n;
+console.log(result);
+
+//завдання 3
+let pen =  5.25;
+let pensil = 3.50;
+let suma = 4*pen + 6*pensil;
+document.write(`Suma ${suma}`);
